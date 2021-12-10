97dac2ac77ef707888fdfef826c9d6f0c64f2d0b
LucasGTorres <lucas.gregtorres@gmail.com>
2021-12-10 14:52:37 -0300

    1st commit

---
+++ src/main/java/com/example/apijuroscompostos/ApiJurosCompostosApplication.java
@@ -0,0 +1,13 @@
+package com.example.apijuroscompostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJurosCompostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJurosCompostosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apijuroscompostos/ApiJurosCompostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apijuroscompostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJurosCompostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
